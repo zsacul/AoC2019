a30fefb1eadf26d01edad7b98c6d75549c275035
zsacul <zsacul@gmail.com>
2019-12-05 00:13:17 +0100

    day4 done

---
+++ day04_1/src/main.rs
@@ -0,0 +1,18 @@
+fn ok(num:u32)->bool {    
+    let tab:Vec<char> = num.to_string().chars().collect();
+    let mut b_same = false;
+    for i in 0..tab.len()-1 {
+        if tab[i+1]< tab[i] { return false; }
+        if tab[i+1]==tab[i] { b_same = true;}
+    }
+    b_same
+}
+
+fn main() {
+    let mut res = 0;
+    for i in 138307..=654504 {
+        if ok(i) { res+=1; }
+    }
+    println!("{}",res);
+}
+
